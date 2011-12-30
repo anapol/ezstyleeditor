1554b9c240bacd90e745e7e6bcd9e9df446b7894
Petr Mifek <petr.mifek@anapol.cz>
2011-12-30 21:45:06 +0100

    Fixed and completed Czech translation for ezstyleeditor.

--- translations/cze-CZ/translation.ts
+++ translations/cze-CZ/translation.ts
@@ -1,125 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
-<context>
-    <name>design/standard/datatype/edit/ezcssstyle</name>
-    <message>
-        <source>New CSS rule</source>
-        <translation type="obsolete">Nové CSS pravidlo</translation>
-    </message>
-    <message>
-        <source>Alias:</source>
-        <translation type="obsolete">Alias:</translation>
-    </message>
-    <message>
-        <source>Selector:</source>
-        <translation type="obsolete">Volič:</translation>
-    </message>
-    <message>
-        <source>Create style</source>
-        <translation type="obsolete">Vytvořit styl</translation>
-    </message>
-    <message>
-        <source>Add property</source>
-        <translation type="obsolete">Přidat vlastnost</translation>
-    </message>
-    <message>
-        <source>Property:</source>
-        <translation type="obsolete">Vlastnosti:</translation>
-    </message>
-    <message>
-        <source>Style:</source>
-        <translation type="obsolete">Styl:</translation>
-    </message>
-    <message>
-        <source>Add</source>
-        <translation type="obsolete">Přidat</translation>
-    </message>
-    <message>
-        <source>Move up.</source>
-        <translation type="obsolete">Jít nahoru.</translation>
-    </message>
-    <message>
-        <source>Move down.</source>
-        <translation type="obsolete">Jít dolů.</translation>
-    </message>
-    <message>
-        <source>Choose image</source>
-        <translation type="obsolete">Vybrat obrázek</translation>
-    </message>
-    <message>
-        <source>Remove image</source>
-        <translation type="obsolete">Vyjmout obrázek</translation>
-    </message>
-    <message>
-        <source>Remove properties</source>
-        <translation type="obsolete">Vyjmout vlastnosti
-</translation>
-    </message>
-    <message>
-        <source>Remove rules</source>
-        <translation type="obsolete">Vyjmout pravidla</translation>
-    </message>
-    <message>
-        <source>Export</source>
-        <translation type="obsolete">Exportovat</translation>
-    </message>
-    <message>
-        <source>Format:</source>
-        <translation type="obsolete">Formát:</translation>
-    </message>
-    <message>
-        <source>XML</source>
-        <translation type="obsolete">XML</translation>
-    </message>
-    <message>
-        <source>CSS</source>
-        <translation type="obsolete">CSS</translation>
-    </message>
-    <message>
-        <source>JSON</source>
-        <translation type="obsolete">Nové CSS pravidlo</translation>
-    </message>
-</context>
-<context>
-    <name>design/standard/datatype/edit/ezcssstyle_images</name>
-    <message>
-        <source>Current image</source>
-        <translation type="obsolete">Stávající obrázek</translation>
-    </message>
-    <message>
-        <source>No image selected.</source>
-        <translation type="obsolete">Není vybrán žádný obrázek.</translation>
-    </message>
-    <message>
-        <source>Available images</source>
-        <translation type="obsolete">Obrázky k dispozici</translation>
-    </message>
-    <message>
-        <source>Set</source>
-        <translation type="obsolete">Nastavit</translation>
-    </message>
-    <message>
-        <source>Upload new image</source>
-        <translation type="obsolete">Nahrát nový obrázek</translation>
-    </message>
-    <message>
-        <source>Name:</source>
-        <translation type="obsolete">Jméno:</translation>
-    </message>
-    <message>
-        <source>File:</source>
-        <translation type="obsolete">Soubor:</translation>
-    </message>
-    <message>
-        <source>Upload</source>
-        <translation type="obsolete">Nahrát</translation>
-    </message>
-    <message>
-        <source>Back</source>
-        <translation type="obsolete">Zpět</translation>
-    </message>
-</context>
+<TS version="2.0" language="cs_CZ">
 <context>
     <name>design/standard/ezcssstyle</name>
     <message>
@@ -128,7 +9,7 @@
     </message>
     <message>
         <source>Remove</source>
-        <translation>Vyjmout</translation>
+        <translation>Odebrat</translation>
     </message>
     <message>
         <source>Save</source>
@@ -136,7 +17,7 @@
     </message>
     <message>
         <source>Apply</source>
-        <translation>Aplikovat</translation>
+        <translation>Použít</translation>
     </message>
     <message>
         <source>Cancel</source>
@@ -152,19 +33,19 @@
     </message>
     <message>
         <source>Elements</source>
-        <translation>Základní prvky</translation>
+        <translation>Prvky</translation>
     </message>
     <message>
         <source>Help</source>
-        <translation>Pomoc</translation>
+        <translation>Nápověda</translation>
     </message>
     <message>
         <source>Exit</source>
-        <translation>Opustit/Odejít</translation>
+        <translation>Ukončit</translation>
     </message>
     <message>
         <source>Properties for</source>
-        <translation>Vlastnosti pro</translation>
+        <translation>Vlastnosti</translation>
     </message>
     <message>
         <source>Property list is empty.</source>
@@ -180,50 +61,50 @@
     </message>
     <message>
         <source>Do you really want to remove selected properties?</source>
-        <translation>Opravdu si přejete vyjmout vybrané vlastnosti?</translation>
+        <translation>Opravdu si přejete odebrat vybrané vlastnosti?</translation>
     </message>
     <message>
         <source>Do you really want to remove selected image?</source>
-        <translation>Opravdu si přejete vyjmout vybraný obrázek?</translation>
+        <translation>Opravdu si přejete odebrat vybraný obrázek?</translation>
     </message>
     <message>
         <source>Restore</source>
-        <translation type="unfinished"></translation>
+        <translation>Obnovit</translation>
     </message>
     <message>
         <source>Create</source>
-        <translation type="unfinished"></translation>
+        <translation>Vytvořit</translation>
     </message>
     <message>
         <source>Version list</source>
-        <translation type="unfinished"></translation>
+        <translation>Seznam verzí</translation>
     </message>
     <message>
         <source>Rules for</source>
-        <translation type="unfinished"></translation>
+        <translation>Pravidla pro</translation>
     </message>
     <message>
         <source>Version</source>
-        <translation type="unfinished"></translation>
+        <translation>Verze</translation>
     </message>
     <message>
         <source>Created</source>
-        <translation type="unfinished"></translation>
+        <translation>Vytvořeno</translation>
     </message>
     <message>
         <source>Modified</source>
-        <translation type="unfinished"></translation>
+        <translation>Změněno</translation>
     </message>
     <message>
         <source>Site style list</source>
-        <translation type="unfinished"></translation>
+        <translation>Seznam stylů stránek</translation>
     </message>
 </context>
 <context>
     <name>extension/ezstyleeditor</name>
     <message>
         <source>Image repository for given path / node ID does not exist. Please check ezstyleeditor.ini INI file configuration.</source>
-        <translation type="unfinished"></translation>
+        <translation>Úložiště obrázků pro danou cestu/číslo uzlu neexistuje. Prosím zkontrolujte konfiguraci v ezstyleeditor.ini.</translation>
     </message>
 </context>
 </TS>
